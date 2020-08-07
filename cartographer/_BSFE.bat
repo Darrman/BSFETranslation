322520e10884add077161c59a57d100ba788db41
Darrman <30606600+Darrman@users.noreply.github.com>
2020-08-08 00:04:07 +0100

    Added phase messages to ep1 and 2
    Now the text in all of these files are consistent. Now, to work on insertion.

--- cartographer/_BSFE.bat
+++ cartographer/_BSFE.bat
@@ -1,5 +1,5 @@
-::cartographer "BSFE1.bs" BSFE1Commands.txt BSFE1Script -s
-::cartographer "BSFE2.bs" BSFE2Commands.txt BSFE2Script -s
+cartographer "BSFE1.bs" BSFE1Commands.txt BSFE1Script -s
+cartographer "BSFE2.bs" BSFE2Commands.txt BSFE2Script -s
 cartographer "BSFE3.bs" BSFE3Commands.txt BSFE3Script -s
-::cartographer "BSFE4.bs" BSFE4Commands.txt BSFE4Script -s
+cartographer "BSFE4.bs" BSFE4Commands.txt BSFE4Script -s
 pause
