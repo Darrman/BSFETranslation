9818a2bd51f4d5120bf94b4e45b97de7cc1e9a3e
Darrman <30606600+Darrman@users.noreply.github.com>
2020-08-09 18:27:53 +0100

    Finished Episode 2's menu text
    Terrain names are so annoying in this game. Half the menus down, half of them to go.

--- atlas/_BSFEEN.bat
+++ atlas/_BSFEEN.bat
@@ -1,5 +1,5 @@
-Atlas -d "log.log" BSFE1Eng.sfc BSFE1Script.txt             
-::Atlas -d "log.log" BSFE2Eng.sfc BSFE2Script.txt             
+::Atlas -d "log.log" BSFE1Eng.sfc BSFE1Script.txt             
+Atlas -d "log.log" BSFE2Eng.sfc BSFE2Script.txt             
 ::Atlas -d "log.log" BSFE3Eng.sfc BSFE3Script.txt             
 ::Atlas -d "log.log" BSFE4Eng.sfc BSFE4Script.txt             
 
