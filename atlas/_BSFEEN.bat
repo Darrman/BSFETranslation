63b85f5acbcdcf99a011794f48d243e0e1d9a4fc
Darrman <30606600+Darrman@users.noreply.github.com>
2020-08-11 00:24:51 +0100

    Episode 3 complete
    I had to spend a good while wrestling with the text. Then I found out I forgot to comment a Japanese line out and I missed parts of the control codes. Oops. Either way, one episode to go and BSFE is 100% translated!

--- atlas/_BSFEEN.bat
+++ atlas/_BSFEEN.bat
@@ -1,6 +1,6 @@
 ::Atlas -d "log.log" BSFE1Eng.sfc BSFE1Script.txt             
-Atlas -d "log.log" BSFE2Eng.sfc BSFE2Script.txt             
-::Atlas -d "log.log" BSFE3Eng.sfc BSFE3Script.txt             
+::Atlas -d "log.log" BSFE2Eng.sfc BSFE2Script.txt             
+Atlas -d "log.log" BSFE3Eng.sfc BSFE3Script.txt             
 ::Atlas -d "log.log" BSFE4Eng.sfc BSFE4Script.txt             
 
 pause
